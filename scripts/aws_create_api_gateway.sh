3863ac4e70c34ffbe3f5ad29ce3e734746f24376
Sergey <edelweissco@gmail.com>
2022-06-28 18:39:42 +0300

    Adds additional quotes to api script

--- scripts/aws_create_api_gateway.sh
+++ scripts/aws_create_api_gateway.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-cd terraform
+cd && cd terraform
 terraform init
 terraform apply --auto-approve
 BASE_URL=$(terraform output -raw base_url)
 echo $BASE_URL
-curl --request POST --url 'https://api.telegram.org/bot"$TELEGRAM_BOT_KEY"/setWebhook' --header 'content-type: application/json' --data '{"url": "$BASE_URL"}'
+curl --request POST --url 'https://api.telegram.org/bot"$TELEGRAM_BOT_KEY"/setWebhook' --header 'content-type: application/json' --data '{"url": \""$BASE_URL\""}'
