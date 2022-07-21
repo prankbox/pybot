7a763e46edad62a9f9f1aa3ae191d7ecfeadc2da
Sergey <edelweissco@gmail.com>
2022-07-21 20:08:42 +0300

    Adds some changes

--- scripts/aws_push_image.sh
+++ scripts/aws_push_image.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-aws ecr get-login-password --region ${REGION}| docker login --username AWS --password-stdin $AWS_ACCOUNT.dkr.ecr.$REGION.amazonaws.com
+aws ecr get-login-password --region $REGION| docker login --username AWS --password-stdin $AWS_ACCOUNT.dkr.ecr.$REGION.amazonaws.com
 docker tag bot:$BUILD_TAG $AWS_ACCOUNT.dkr.ecr.$REGION.amazonaws.com/bot:$BUILD_TAG
 docker push $AWS_ACCOUNT.dkr.ecr.$REGION.amazonaws.com/bot:$BUILD_TAG
